b9b5c7176de69efcb708c4698ee094047875b7d4
evanguerra <guerrae@protonmail.com>
2024-04-30 16:13:06 -0500

    Updated functions to run with real data

--- smell_loss_simulation.py
+++ smell_loss_simulation.py
@@ -1,61 +1,68 @@
 import numpy as np
 import pandas as pd
 
+
 class Person:
-    def __init__(self, age, covid_status, location, congenital_smell_loss_prob=0.01):
+    def __init__(self, age, location, infected_status='none'):
+        self.location = location
         self.age = age
-        self.covid_status = covid_status
-        self.congenital_smell_loss_prob = congenital_smell_loss_prob
+        self.infected_status = infected_status
+        self.congenital_smell_loss_prob = 0.01
         self.smell_loss = False
         self.is_smoker = False
-        self.location = location
 
-        # Assign congenital smell loss
         if np.random.rand() < self.congenital_smell_loss_prob:
             self.smell_loss = True
 
-    def get_infected(self, virus_type, smell_loss_prob):
-        if virus_type == 'covid':
-            # Define infection probability based on age
-            infection_prob = calculate_covid_infection_probability(self.age)
-        else:
-            # Define infection probability for non-covid virus
-            infection_prob = calculate_non_covid_infection_probability(self.age)
+    def get_infected(self, virus_type, smell_loss_prob, infection_prob):
 
-        # Simulate infection and smell loss
         if np.random.rand() < infection_prob:
+            self.infected_status = virus_type
             if np.random.rand() < smell_loss_prob:
                 self.smell_loss = True
 
 
-def calculate_covid_infection_probability(age):
+def calculate_covid_infection_probability(year):
     """
-    Calculates the covid infection probability for a person based on age
-    :param age: age of the person
+    Calculates the covid infection probability for a person
     :return: covid infection probability
     """
-    # TODO update this with actual calculation
-    return 0.1
+    data = pd.read_csv('WHO-COVID-19-global-data.csv')
+
+    data['Date_reported'] = pd.to_datetime(data['Date_reported'])
+    filtered_data = data[(data['Country'] == 'United States') & (data['Date_reported'].dt.year <= year)]
+
+    cases = filtered_data['New_cases'].sum()
+    covid_prob = cases / 334998000
+
+    return covid_prob
 
 
-def calculate_non_covid_infection_probability(age):
+def calculate_non_covid_infection_probability(year):
     """
-        Calculates the non-covid infection probability for a person based on age
-        :param age: age of the person
+        Calculates the non-covid infection probability for a person
         :return: non-covid infection probability
         """
-    # TODO update this with actual calculation
-    return 0.05
+    data = load_smell_loss_data(year)
+
+    non_covid_prob = (data['CSQ020'] == 1).mean()
+
+    return non_covid_prob
 
 
-def calculate_smell_loss_probability(age):
+def calculate_smell_loss_probability(year):
     """
-        Calculates the smell loss probability for a person based on age and smoker status
-        :param age: age of the person
+        Calculates the smell loss probability for a person based
         :return: smell loss probability
         """
-    # TODO update this with actual calculation
-    return 0.2
+    data = load_smell_loss_data(year)
+
+    if year == 2021:
+        smell_loss_prob = (data['Symptoms_changes_in_smell'] == 1).mean
+    else:
+        smell_loss_prob = (data['CSQ010'] == 1).mean()
+
+    return smell_loss_prob
 
 
 def load_smell_loss_data(year):
@@ -66,7 +73,7 @@ def load_smell_loss_data(year):
         data = pd.read_csv('NHANES20112012/2011_smell_loss_data.csv')
     elif year == 2014:
         data = pd.read_csv('NHANES20132014/2014_smell_loss_data.csv')
-    elif year == 2020 or year == 2021:
+    elif year == 2021:
         data = pd.read_csv('GCCR002/data-clean.csv')
     else:
         raise ValueError("Invalid year provided")
@@ -82,36 +89,39 @@ def move_people(population):
         person.location += np.random.randint(-3, 4)
 
 
-def run_simulation(population_size, num_iterations, transmission_distance=10):
+def run_simulation(population_size, num_iterations, transmission_distance=10, year=2011, initial_infected=10):
     """
     Run the simulation with the given population size and number of iterations and transmission distance
     """
     population = []
+    if year == 2021:
+        infection_prob = calculate_covid_infection_probability(year)
+        virus = 'covid'
+    else:
+        # Define infection probability for non-covid virus
+        infection_prob = calculate_non_covid_infection_probability(year)
+        virus = 'flu_cold'
 
-    # Load smell loss data for COVID pandemic
-    covid_smell_loss_data = load_smell_loss_data(2020)
+    smell_loss_probability = calculate_smell_loss_probability(year)
 
-    # Generate population
     for _ in range(population_size):
         age = np.random.randint(1, 101)
-        # TODO update this with real initial infection population
-        covid_status = np.random.choice(['infected', 'not_infected'],
-                                        p=[0.1, 0.9])
-        location = np.random.randint(0, 100)
-        person = Person(age, covid_status, location)
+        infected_status = virus if initial_infected > 0 else 'none'
+        initial_infected -= 1
+        locale = np.random.randint(0, 100)
+        person = Person(age, locale, infected_status)
         population.append(person)
 
-    # Run simulation
     for _ in range(num_iterations):
         move_people(population)
         for i, person in enumerate(population):
             for j, other_person in enumerate(population):
                 if i != j:
                     if np.abs(person.location - other_person.location) <= transmission_distance:
-                        if person.covid_status == 'infected':
-                            person.get_infected('covid', calculate_smell_loss_probability(person.age))
-                        elif other_person.covid_status == 'infected':
-                            other_person.get_infected('covid', calculate_smell_loss_probability(other_person.age))
+                        if person.infected_status != 'none':
+                            person.get_infected(virus, smell_loss_probability, infection_prob)
+                        elif other_person.infected_status != 'none':
+                            other_person.get_infected(virus, smell_loss_probability, infection_prob)
 
     smell_loss_counts = sum(person.smell_loss for person in population)
     total_population = len(population)
@@ -125,5 +135,11 @@ if __name__ == "__main__":
     iterations = 100
     infection_distance = 5
 
-    smell_loss, population = run_simulation(pop_size, iterations, infection_distance)
-    print(f"Percentage of population with smell loss: {smell_loss:.2f}%")
+    smell_loss, population = run_simulation(pop_size, iterations, infection_distance, year=2011)
+    print(f"Percentage of population with smell loss (Year 2011): {smell_loss:.2f}%")
+
+    smell_loss, population = run_simulation(pop_size, iterations, infection_distance, year=2014)
+    print(f"Percentage of population with smell loss (Year 2014): {smell_loss:.2f}%")
+
+    smell_loss, population = run_simulation(pop_size, iterations, infection_distance, year=2021)
+    print(f"Percentage of population with smell loss (Year 2021): {smell_loss:.2f}%")
