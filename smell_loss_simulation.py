9e8e92c311cdfa6786f2034762e1752001eaaa18
evanguerra <guerrae@protonmail.com>
2024-04-30 22:26:28 -0500

    updated docstrings

--- smell_loss_simulation.py
+++ smell_loss_simulation.py
@@ -12,6 +12,12 @@ class Person:
     all_persons = []
 
     def __init__(self, age, location, infected_status='none'):
+        """
+        Initialize the person class with an age and a location and infected status
+        :param age: The age of the person
+        :param location: The starting location of the person
+        :param infected_status: The status of the infected person (either 'none', 'covid', or 'flu_cold'
+        """
         Person.person_count += 1
         Person.all_persons.append(self)
         self.location = location
@@ -25,7 +31,13 @@ class Person:
             self.smell_loss = True
 
     def get_infected(self, virus_type, smell_loss_prob, infection_prob):
-
+        """
+        Update the infected status of the person based on the infection rate and smell loss rate
+        :param virus_type: The virus type of the person (either 'none', 'covid', or 'flu_cold'
+        :param smell_loss_prob: The smell loss rate of the population
+        :param infection_prob: The infection rate of the population
+        :return: None
+        """
         if np.random.rand() < infection_prob:
             self.infected_status = virus_type
             if np.random.rand() < smell_loss_prob:
@@ -54,6 +66,7 @@ class Person:
 def calculate_covid_infection_probability(year):
     """
     Calculates the covid infection probability for a person
+    :param year: The year for the simulation
     :return: covid infection probability
     """
     cases = load_case_data(year)
@@ -66,6 +79,7 @@ def calculate_covid_infection_probability(year):
 def calculate_non_covid_infection_probability(year):
     """
         Calculates the non-covid infection probability for a person
+        :param year: The year for the simulation
         :return: non-covid infection probability
         """
     data = load_smell_loss_data(year)
@@ -77,7 +91,8 @@ def calculate_non_covid_infection_probability(year):
 
 def calculate_smell_loss_probability(year):
     """
-        Calculates the smell loss probability for a person based
+        Calculates the smell loss probability for a person based on the smell loss rate of the virus type
+        :param year: The year for the simulation
         :return: smell loss probability
         """
     data = load_smell_loss_data(year)
@@ -97,6 +112,8 @@ def calculate_smell_loss_probability(year):
 def load_smell_loss_data(year):
     """
     Loads the smell loss data from the csv file and returns it as a pandas dataframe
+    :param year: The year for the simulation
+    :return: the dataframe
     """
     if year == 2011:
         data = pd.read_csv('NHANES20112012/2011_smell_loss_data.csv')
@@ -111,6 +128,11 @@ def load_smell_loss_data(year):
 
 
 def load_case_data(year):
+    """
+    Loads the covid case data from the csv file and returns it as a pandas dataframe
+    :param year: The year for the simulation
+    :return: the dataframe
+    """
     data = pd.read_csv('WHO-COVID-19-global-data.csv')
     data['Date_reported'] = pd.to_datetime(data['Date_reported'])
     filtered_data = data[(data['Country'] == 'United States of America') & (data['Date_reported'].dt.year <= year)]
@@ -122,6 +144,7 @@ def load_case_data(year):
 def move_people(population):
     """
     Move the people from one location to another
+    :param population: The population of the simulation
     """
     for person in population:
         person.location += np.random.randint(-3, 4)
@@ -130,6 +153,12 @@ def move_people(population):
 def run_simulation(population_size, num_iterations, transmission_distance=10, year=2011, initial_infected=10):
     """
     Run the simulation with the given population size and number of iterations and transmission distance
+    :param population_size: The population size of the simulation
+    :param num_iterations: The number of iterations of the simulation
+    :param transmission_distance: The transmission distance of the simulation
+    :param year: The year of the simulation
+    :param initial_infected: The initial infected persons in the simulation
+    :return: None
     """
     population = []
     aggregate_stats = []
